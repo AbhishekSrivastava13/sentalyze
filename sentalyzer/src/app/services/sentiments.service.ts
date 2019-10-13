a3b399e0daec9d06ce8cf1451b2009a535017db1
iQrus <39492397+iQrus@users.noreply.github.com>
2019-10-14 00:10:17 +0530

    code to fetch list of sentiments from backend service on click of search

---
+++ sentalyzer/src/app/services/sentiments.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SentimentsService {
+
+  url = 'http://localhost:8090/getSentiments';
+
+  constructor(private http: HttpClient) {}
+
+  getSentiments(keyword: string): Observable<any> {
+    return this.http.get(`${this.url}?keyword=${encodeURI(keyword)}`).pipe(
+      map(results => {
+        console.log('Raw results : ',results);
+        return results;
+      })
+    );
+  }
+}
