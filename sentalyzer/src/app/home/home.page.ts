8ef277fbba19e08322df0b6540141f53f7978077
shiny21 <shivanipriyanka17@gmail.com>
2019-10-19 21:36:03 +0530

    barabar krna

--- sentalyzer/src/app/home/home.page.ts
+++ sentalyzer/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SentimentsService } from '../services/sentiments.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  results: Observable<any>;
+  searchTerm: string = '';
+  sentimentResponseMap: {'text inside map ': {
+    'magnitude' : '0.8',
+    'score' : '0.5'
+  }};
+  constructor(private sentimentService: SentimentsService) {}
+
+  getSentiments(){
+    console.log('Search term : ',this.searchTerm);
+    this.results = this.sentimentService.getSentiments(this.searchTerm);
+    console.log('Results in Main : ',this.results);
+    /* this.results.subscribe(res => {
+      console.log('inside subscribe : ',res);
+      this.sentimentResponseMap = res.sentimentResponseMap;
+      console.log('sentiment response map : ',this.sentimentResponseMap);
+    }) */
+  }
 
 }
